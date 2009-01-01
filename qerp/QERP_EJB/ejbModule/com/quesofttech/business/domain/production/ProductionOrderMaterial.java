5744017a86fd00841971a398dcae77cc313dada5
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-01 10:46:05 +0000

    Initial commit of QERP_EJB files

---
+++ qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/production/ProductionOrderMaterial.java
@@ -0,0 +1,459 @@
+package com.quesofttech.business.domain.production;
+
+import java.io.Serializable;
+//import java.sql.Date;
+//import java.sql.Time;
+import java.sql.Timestamp;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.PostLoad;
+import javax.persistence.PrePersist;
+import javax.persistence.PostPersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
+import javax.persistence.TableGenerator;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.Version;
+import javax.persistence.Embedded;
+//import javax.persistence.SequenceGenerator;
+
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.domain.base.BaseEntity;
+import com.quesofttech.business.domain.embeddable.RowInfo;
+import com.quesofttech.business.domain.sales.SalesOrder;
+import com.quesofttech.business.domain.inventory.Material;
+import com.quesofttech.business.common.exception.ValueRequiredException;
+import com.quesofttech.business.common.exception.GenericBusinessException;
+import com.quesofttech.util.StringUtil;
+
+import java.util.List;
+//import java.util.ArrayList;
+
+@Entity
+@Table(name = "ProductionOrderMaterial", uniqueConstraints = { @UniqueConstraint(columnNames = { "fk_ProductionOrder","fk_ProductionOrderOperation","fk_Material" }) })
+@SuppressWarnings("serial")
+public class ProductionOrderMaterial extends BaseEntity {
+	
+
+	//For Postgresql : @SequenceGenerator(name = "ProductionOrderMaterial_sequence", sequenceName = "ProductionOrderMaterial_id_seq")
+	//Generic solution : (Use a table named primary_keys, with 2 fields , key &  value)
+	@TableGenerator(  name="ProductionOrderMaterial_id", table="PrimaryKeys", pkColumnName="tableName", pkColumnValue="ProductionOrderMaterial", valueColumnName="keyField")
+	@Id
+	//For Postgresql : @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ProductionOrderMaterial_sequence")
+	//For MSSQL      : @GeneratedValue(strategy = GenerationType.IDENTITY)
+	//Generic solution :
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "ProductionOrderMaterial_id")	
+	@Column(name = "id_ProductionOrderMaterial", nullable = false)
+	private Long id;
+	
+	@Version
+	@Column(nullable = false)
+	private Long version;
+
+
+	// Example of field	
+	//@Column(name = "matt_Type", length = 1, nullable = false)
+	//private String type;
+	@Column(name = "prodom_QuantityRequired", nullable = false)
+	private Double quantityRequired;
+		
+	@Column(name = "prodom_QuantityConsumed", nullable = false)
+	private Double quantityConsumed;
+	
+	/*
+	@Column(name = "prodom_Sequence", nullable = false)
+	private Double sequence;
+	*/
+	
+	
+	@Embedded
+	RowInfo rowInfo;
+	
+	// Foreign keys
+	@ManyToOne
+    @JoinColumn(name="fk_ProductionOrder")	
+	private ProductionOrder productionOrder;
+	
+	@ManyToOne
+    @JoinColumn(name="fk_ProductionOrderOperation")	
+	private ProductionOrderOperation productionOrderOperation;	
+	
+	
+	// Foreign keys
+	@ManyToOne
+    @JoinColumn(name="fk_Material")	
+	private Material material;
+	
+	//@OneToMany(cascade=CascadeType.ALL, fetch = FetchType.EAGER, mappedBy="ProductionOrderMaterial", targetEntity=<ForeignTable>.class)
+	//private List<<ForeignTable>> <foreignTable>s; // = new List<<ForeignTable>>();
+	
+	public ProductionOrderMaterial() {
+		super();
+		this.rowInfo = new RowInfo();
+	}	
+
+	
+	// Constructors without the common fields
+	//public ProductionOrderMaterial(/* all fields */) {
+	//	super();
+		/* Example of assignment
+		this.id = id;
+		this.type = type;
+		this.description = description;
+		*/
+	//}
+
+
+	// Constructors with all fields
+	public ProductionOrderMaterial(/* all fields*/
+			String recordStatus, String sessionId, String createLogin,
+			String createApp, Timestamp createTimestamp,
+			String modifyLogin, String modifyApp, Timestamp modifyTimestamp) {
+		this();
+
+		
+		this.id = id;
+		/* Example of assignment
+		this.type = type;
+		this.description = description;
+		*/
+
+
+		this.rowInfo.setRecordStatus(recordStatus);
+		this.rowInfo.setSessionId(sessionId);
+		this.rowInfo.setCreateLogin(createLogin);
+		this.rowInfo.setCreateApp(createApp);
+		this.rowInfo.setCreateTimestamp(createTimestamp);
+		this.rowInfo.setModifyLogin(modifyLogin);
+		this.rowInfo.setModifyApp(modifyApp);
+		this.rowInfo.setModifyTimestamp(modifyTimestamp);
+	}
+
+
+	@Override
+	public String toString() {
+		
+		StringBuffer buf = new StringBuffer();
+		buf.append("ProductionOrderMaterial: [");
+		buf.append("id=" + id + ", ");
+
+		/* All fields
+		buf.append("type=" + type + ", ");
+		buf.append("description=" + description + ", ");
+		buf.append("isProduced=" + isProduced + ", ");
+		buf.append("isPurchased=" + isPurchased + ", ");
+		buf.append("isForSales=" + isForSale + ", ");
+		*/
+		// Common fields
+		buf.append("record status=" + rowInfo.getRecordStatus() + ", ");
+		buf.append("session id=" + rowInfo.getSessionId() + ", ");
+		buf.append("create login=" + rowInfo.getCreateLogin() + ", ");
+		buf.append("create app=" + rowInfo.getCreateApp() + ", ");
+		buf.append("create timestamp=" + rowInfo.getCreateTimestamp() + ", ");
+		buf.append("modify login=" + rowInfo.getModifyLogin() + ", ");
+		buf.append("modify app=" + rowInfo.getModifyApp() + ", ");
+		buf.append("modify time=" + rowInfo.getModifyTimestamp() + ", ");		
+		buf.append("version=" + version);
+		buf.append("]");
+		return buf.toString();		
+			
+	}
+
+	// The need for an equals() method is discussed at http://www.hibernate.org/109.html
+
+	@Override
+	public boolean equals(Object obj) {
+		return (obj == this) || (obj instanceof ProductionOrderMaterial) && getId() != null && ((ProductionOrderMaterial) obj).getId().equals(this.getId());
+	}
+
+	// The need for a hashCode() method is discussed at http://www.hibernate.org/109.html
+
+	@Override
+	public int hashCode() {
+		return getId() == null ? super.hashCode() : getId().hashCode();
+	}	
+	
+	@Override
+	public Serializable getIdForMessages() {
+		return getId();
+	}
+
+	
+	@PrePersist
+	void prePersist() throws BusinessException {
+		validate();
+		
+		rowInfo.setRecordStatus("A");
+		
+	    java.util.Date today = new java.util.Date();
+
+	    rowInfo.setModifyTimestamp(new java.sql.Timestamp(today.getTime()));
+	    rowInfo.setCreateTimestamp(rowInfo.getModifyTimestamp());			
+				
+	}
+
+	@PostPersist
+	void postPersist() throws BusinessException {
+
+	}
+	
+	@PostLoad
+	void postLoad() {
+
+	}
+
+	@PreUpdate
+	void preUpdate() throws BusinessException {
+		if(rowInfo.getRecordStatus()!="D")
+		{
+			validate();
+		}
+			java.util.Date today = new java.util.Date();
+
+			rowInfo.setModifyTimestamp(new java.sql.Timestamp(today.getTime()));
+		
+		
+	}
+
+	@PreRemove
+	void preRemove() throws BusinessException {
+		// Check business rules here, eg.
+		// if (entity.getParts().size() > 0) {
+		// throw new CannotDeleteIsNotEmptyException(this, id, "Part");
+		// }
+
+		// There's no need to remove me from the parent(s) - that's the caller's
+		// responsibility (and for performance, it might not bother)
+	}
+
+	public void validate() throws BusinessException {
+
+		
+		// Validate syntax...
+
+		if (this.material == null) {
+			//System.out.println("Yeah");
+			throw new ValueRequiredException(this, "Material");
+		}
+
+		if (quantityRequired == null||quantityRequired==0) {
+			//System.out.println("Yeah");
+			throw new ValueRequiredException(this, "ProductionOrderMaterial_QuantityOrder");
+		}
+		
+		//if (StringUtil.isEmpty(description)) {
+		//	throw new ValueRequiredException(this, "MaterialType_Description");
+		//}
+
+		/*
+		if (StringUtil.isEmpty(lastName)) {
+			throw new ValueRequiredException(this, "User_lastName");
+		}
+		*/
+
+		// Validate semantics...
+
+		if (this.quantityRequired <=0) {
+			throw new GenericBusinessException("ProductionOrder_QuantityOrderMustGreaterThanZero", new Object[] { });
+		}
+		
+		//throw new ValueRequiredException(this, this.getClass().getName() + " [validate] Please remove this line and code the validation logic");
+	}
+	
+	
+
+	// Common EJB field
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+
+
+
+	public String getRecordStatus() {
+		return rowInfo.getRecordStatus();
+	}
+
+
+
+	public void setRecordStatus(String recordStatus) {
+		this.rowInfo.setRecordStatus(recordStatus);
+	}
+
+
+
+	public String getSessionId() {
+		return rowInfo.getSessionId();
+	}
+
+
+	public void setSessionId(String sessionId) {
+		this.rowInfo.setSessionId(sessionId);
+	}
+
+
+
+	public String getCreateLogin() {
+		return rowInfo.getCreateLogin();
+	}
+
+
+
+	public void setCreateLogin(String createLogin) {
+		this.rowInfo.setCreateLogin(createLogin);
+	}
+
+
+
+	public String getCreateApp() {
+		return rowInfo.getCreateApp();
+	}
+
+
+
+	public void setCreateApp(String createApp) {
+		this.rowInfo.setCreateApp(createApp);
+	}
+
+
+
+	public java.sql.Timestamp getCreateTimestamp() {
+		return rowInfo.getCreateTimestamp();
+	}
+
+	public void setCreateTimestamp(java.sql.Timestamp createTimestamp) {
+		this.rowInfo.setCreateTimestamp(createTimestamp);
+	}
+
+
+	public String getModifyLogin() {
+		return rowInfo.getModifyLogin();
+	}
+
+	public void setModifyLogin(String modifyLogin) {
+		this.rowInfo.setModifyLogin(modifyLogin);
+	}
+
+
+
+	public String getModifyApp() {
+		return rowInfo.getModifyApp();
+	}
+
+
+
+	public void setModifyApp(String modifyApp) {
+		this.rowInfo.setModifyApp(modifyApp);
+	}
+
+
+	public java.sql.Timestamp getModifyTimestamp() {
+		return rowInfo.getModifyTimestamp();
+	}
+
+
+
+	public void setModifyTimestamp(java.sql.Timestamp modifyTimestamp) {
+		this.rowInfo.setModifyTimestamp(modifyTimestamp);
+	}
+
+
+	/**
+	 * @return the quantityRequired
+	 */
+	public Double getQuantityRequired() {
+		return quantityRequired;
+	}
+
+
+	/**
+	 * @param quantityRequired the quantityOrder to set
+	 */
+	public void setQuantityRequired(Double quantityRequired) {
+		this.quantityRequired = quantityRequired;
+	}
+
+
+	/**
+	 * @return the quantityConsumed
+	 */
+	public Double getQuantityConsumed() {
+		return quantityConsumed;
+	}
+
+
+	/**
+	 * @param quantityConsumed the quantityConsumed to set
+	 */
+	public void setQuantityConsumed(Double quantityConsumed) {
+		this.quantityConsumed = quantityConsumed;
+	}
+
+
+	/**
+	 * @return the productionOrder
+	 */
+	public ProductionOrder getProductionOrder() {
+		return productionOrder;
+	}
+
+
+	/**
+	 * @param productionOrder the productionOrder to set
+	 */
+	public void setProductionOrder(ProductionOrder productionOrder) {
+		this.productionOrder = productionOrder;
+	}
+
+
+	/**
+	 * @return the material
+	 */
+	public Material getMaterial() {
+		return material;
+	}
+
+
+	/**
+	 * @param material the material to set
+	 */
+	public void setMaterial(Material material) {
+		this.material = material;
+	}
+
+	public String getMaterialCode()
+	{
+		if(material!=null)			
+			return material.getCode();
+		else
+			return "";
+	}
+	
+}
---
+++ qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/production/ProductionOrderService.java
@@ -0,0 +1,236 @@
+package com.quesofttech.business.domain.production;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.common.exception.DoesNotExistException;
+import com.quesofttech.business.domain.base.BaseService;
+import com.quesofttech.business.domain.production.ProductionOrder;
+import com.quesofttech.business.domain.production.iface.IProductionOrderServiceLocal;
+import com.quesofttech.business.domain.production.iface.IProductionOrderServiceRemote;
+
+import com.quesofttech.business.domain.production.ProductionOrderMaterial;
+import com.quesofttech.business.domain.production.ProductionOrderOperation;
+import com.quesofttech.business.domain.sales.SalesOrder;
+import com.quesofttech.business.domain.sales.SalesOrderMaterial;
+
+
+@Stateless
+@Local(IProductionOrderServiceLocal.class)
+@Remote(IProductionOrderServiceRemote.class)
+public class ProductionOrderService extends BaseService implements IProductionOrderServiceLocal, IProductionOrderServiceRemote {
+
+//	@PersistenceContext(unitName = "QERP_EJB")
+//	protected EntityManager _em;
+
+	// ProductionOrder
+
+	public ProductionOrder findProductionOrder(Long id) throws DoesNotExistException {
+		ProductionOrder productionOrder = (ProductionOrder) find(ProductionOrder.class, id);
+		return productionOrder;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrder> findProductionOrders() throws DoesNotExistException {
+		Query q = _em.createQuery("select productionOrder from ProductionOrder productionOrder where productionOrder.rowInfo.recordStatus='A' order by productionOrder.id ");
+		List l = q.getResultList();
+		return l;
+	}
+/*
+	public ProductionOrder findProductionOrderByType(String type) {
+		ProductionOrder productionOrder = _em.find(ProductionOrder.class, id);
+		return productionOrder;
+	}
+*/
+	
+	
+	public void updateProductionOrder(ProductionOrder productionOrder) throws BusinessException {	
+		System.out.println(productionOrder.toString());
+		productionOrder = (ProductionOrder) merge(productionOrder);
+	}
+
+	public void logicalDeleteProductionOrder(ProductionOrder productionOrder) throws BusinessException {
+		productionOrder.rowInfo.setRecordStatus("D");
+		
+		for(ProductionOrderOperation productionOrderOperation : productionOrder.getProductionOrderOperations())
+		{
+			productionOrderOperation.rowInfo.setRecordStatus("D");
+		}
+		
+		for(ProductionOrderMaterial productionOrderMaterial : productionOrder.getProductionOrderMaterials())
+		{
+			productionOrderMaterial.rowInfo.setRecordStatus("D");
+		}
+		
+		updateProductionOrder(productionOrder);
+	}
+	
+	public void addProductionOrder(ProductionOrder productionOrder) throws BusinessException {
+		
+		//try{
+		System.out.println("just before persist in ProductionOrderService");
+		persist(productionOrder);
+		
+		System.out.println("just after persist in ProductionOrderService");
+		//}
+	/*	catch (java.lang.RuntimeException e)
+		{
+			System.out.println("[ProductionOrderService] RuntimeException caught in persist");
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println("[addProductionOrder] : exception caught during persist" + e.getMessage());
+			throw (BusinessException) e;
+		}
+		*/
+	}
+	
+	// ProductionOrderOperation
+	
+
+	public void addProductionOrderOperation(ProductionOrderOperation productionOrderOperation) throws BusinessException {
+		persist(productionOrderOperation);
+	}
+	
+	public void updateProductionOrderOperation(ProductionOrderOperation productionOrderOperation) throws BusinessException {		
+		productionOrderOperation = (ProductionOrderOperation) merge(productionOrderOperation);
+	}
+	
+	
+	public void logicalDeleteProductionOrderOperation(ProductionOrderOperation productionOrderOperation) throws BusinessException {
+		productionOrderOperation.rowInfo.setRecordStatus("D");
+						
+		updateProductionOrderOperation(productionOrderOperation);
+	}
+		
+	
+	// Finders
+
+	public ProductionOrderOperation findProductionOrderOperation(Long id) throws DoesNotExistException {
+		ProductionOrderOperation productionOrderMaterial = (ProductionOrderOperation) find(ProductionOrderOperation.class, id);
+		return productionOrderMaterial;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrderOperation> findProductionOrderOperations() throws DoesNotExistException {
+		Query q = _em.createQuery("select prodoo from ProductionOrderOperation prodoo where prodoo.rowInfo.recordStatus='A' order by prodoo.id ");
+		List l = q.getResultList();
+		return l;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrderOperation> findProductionOrderOperationsByProductionOrderId(Long productionOrderId) throws DoesNotExistException {
+		//ProductionOrder productionOrder = this.findProductionOrder(productionOrderId);
+		
+		Query q = _em.createQuery("select prodoo from ProductionOrderOperation prodoo where prodoo.productionOrder.id = :productionOrderId AND prodoo.rowInfo.recordStatus='A'" +
+				" order by prodoo.id");
+		q.setParameter("productionOrderId", productionOrderId);
+		List l = q.getResultList();
+		return l;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrderOperation> findProductionOrderOperationsByProductionOrder(ProductionOrder productionOrder) throws DoesNotExistException {
+		
+		Query q = _em.createQuery("select prodoo from ProductionOrderOperation prodoo where prodoo.productionOrder = :productionOrder AND prodoo.rowInfo.recordStatus='A'" +
+				" order by prodoo.id");
+		q.setParameter("productionOrder", productionOrder);
+		List l = q.getResultList();
+		return l;
+	}
+	
+	
+	// ProductionOrderMaterial
+	
+	public void addProductionOrderMaterial(long id,ProductionOrderMaterial productionOrderMaterial) throws BusinessException {
+		//persist(productionOrderMaterial);
+		//ProductionOrderMaterial productionOrderMaterial = new ProductionOrderMaterial();
+		ProductionOrder productionOrder = findProductionOrder(id);
+		/*
+		productionOrderMaterial.setMaterial(productionOrderMaterial.getMaterial());
+		productionOrderMaterial.setQuantityRequired(productionOrderMaterial.getQuantityRequired());
+		productionOrderMaterial.setQuantityConsumed(productionOrderMaterial.getQuantityConsumed());
+		productionOrderMaterial.setRecordStatus("A");
+		*/
+		productionOrder.addProductionOrderMaterial(productionOrderMaterial);
+		
+		
+		/*
+		SalesOrderMaterial salesOrderLine1 = new SalesOrderMaterial();
+		System.out.println("finding id:" + id);
+		SalesOrder salesOrder = findSalesOrder(id);
+		//salesOrderLine1.setSalesOrder(salesOrder);
+		System.out.println("found id:" + id);
+		salesOrderLine1.setLine((salesordermaterial.getLine()));
+		salesOrderLine1.setQuantityOrder(salesordermaterial.getQuantityOrder());
+		salesOrderLine1.setQuantityShipped(salesordermaterial.getQuantityShipped());
+		salesOrderLine1.setCustomerPOLine(salesordermaterial.getCustomerPOLine());
+		salesOrderLine1.setMaterial(salesordermaterial.getMaterial());
+		salesOrder.addSalesOrderMaterial(salesOrderLine1);
+		*/
+		
+		
+	}	
+
+	public void updateProductionOrderMaterial(ProductionOrderMaterial productionOrderMaterial) throws BusinessException {		
+		productionOrderMaterial = (ProductionOrderMaterial) merge(productionOrderMaterial);
+	}
+	
+	
+	public void logicalDeleteProductionOrderMaterial(ProductionOrderMaterial productionOrderMaterial) throws BusinessException {
+		productionOrderMaterial.rowInfo.setRecordStatus("D");
+						
+		updateProductionOrderMaterial(productionOrderMaterial);
+	}
+	
+	
+	
+	
+	
+	// Finders
+
+	public ProductionOrderMaterial findProductionOrderMaterial(Long id) throws DoesNotExistException {
+		ProductionOrderMaterial productionOrderMaterial = (ProductionOrderMaterial) find(ProductionOrderMaterial.class, id);
+		return productionOrderMaterial;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrderMaterial> findProductionOrderMaterials() throws DoesNotExistException {
+		Query q = _em.createQuery("select prodom from ProductionOrderMaterial prodom where prodom.rowInfo.recordStatus='A' order by prodom.id ");
+		List l = q.getResultList();
+		return l;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrderMaterial> findProductionOrderMaterialsByProductionOrderId(Long productionOrderId) throws DoesNotExistException {
+		
+		ProductionOrder productionOrder = this.findProductionOrder(productionOrderId);
+		
+		Query q = _em.createQuery("select prodom from ProductionOrderMaterial prodom where prodom.productionOrder = :productionOrder AND prodom.rowInfo.recordStatus='A'" +
+				" order by prodom.id");
+		q.setParameter("productionOrder", productionOrder);
+		List l = q.getResultList();
+		return l;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ProductionOrderMaterial> findProductionOrderMaterialsByProductionOrder(ProductionOrder productionOrder) throws DoesNotExistException {
+		
+		Query q = _em.createQuery("select prodom from ProductionOrderMaterial prodom where prodom.productionOrder = :productionOrder AND prodom.rowInfo.recordStatus='A'" +
+				" order by prodom.id");
+		q.setParameter("productionOrder", productionOrder);
+		List l = q.getResultList();
+		return l;
+	}
+	
+	
+}
