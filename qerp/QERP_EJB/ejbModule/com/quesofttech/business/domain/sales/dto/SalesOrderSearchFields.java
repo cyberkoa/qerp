961dcaf1fdeb49117447b18de890e0da32300d67
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-01 10:47:23 +0000

    Initial commit of QERP_EJB files

---
+++ qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/sales/dto/SalesOrderSearchFields.java
@@ -0,0 +1,110 @@
+package com.quesofttech.business.domain.sales.dto;
+
+
+import java.io.Serializable;
+
+import com.quesofttech.business.domain.base.dto.BaseSearchFields;
+import com.quesofttech.business.domain.inventory.Material;
+import com.quesofttech.business.domain.sales.SalesOrder;
+import com.quesofttech.business.domain.sales.Customer;
+
+
+//import java.util.Date;
+
+@SuppressWarnings("serial")
+public class SalesOrderSearchFields extends BaseSearchFields implements Serializable {
+
+	private String docNo;
+	private Customer customer = new Customer();
+	private String customerCode = null;
+    private Material material = new Material();
+	private String recordStatus = null;
+	private Integer version = null;
+
+	/*
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("SalesOrderSearchFields: [");
+		buf.append("record status=" + recordStatus + ", ");
+		buf.append("version=" + version);
+		buf.append("]");
+		return buf.toString();
+	}
+    */
+
+
+	/**
+	 * @return the customer
+	 */
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	/**
+	 * @param customer the customer to set
+	 */
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	/**
+	 * @return the material
+	 */
+	public Material getMaterial() {
+		return material;
+	}
+
+	/**
+	 * @param material the material to set
+	 */
+	public void setMaterial(Material material) {
+		this.material = material;
+	}
+
+	/**
+	 * @return the recordStatus
+	 */
+	public String getRecordStatus() {
+		return recordStatus;
+	}
+
+	/**
+	 * @param recordStatus the recordStatus to set
+	 */
+	public void setRecordStatus(String recordStatus) {
+		this.recordStatus = recordStatus;
+	}
+
+	/**
+	 * @return the customerCode
+	 */
+	public String getCustomerCode() {
+		return customerCode;
+	}
+
+	/**
+	 * @param customerCode the customerCode to set
+	 */
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+
+	/**
+	 * @return the docNo
+	 */
+	public String getDocNo() {
+		return docNo;
+	}
+
+	/**
+	 * @param docNo the docNo to set
+	 */
+	public void setDocNo(String docNo) {
+		this.docNo = docNo;
+	}
+
+
+
+
+
+}
