360a9f3c6155d0a7f35e4d1a2820b9974d98e0ce
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-01 10:54:27 +0000

    Initial commit of QERP_EJB files

---
+++ trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/sales/iface/ISalesOrderServiceRemote.java
@@ -0,0 +1,40 @@
+package com.quesofttech.business.domain.sales.iface;
+
+import java.util.List;
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.common.exception.DoesNotExistException;
+import com.quesofttech.business.common.query.SearchOptions;
+import com.quesofttech.business.domain.sales.SalesOrder;
+import com.quesofttech.business.domain.sales.SalesOrderMaterial;
+import com.quesofttech.business.domain.sales.dto.SalesOrderSearchFields;
+
+
+/**
+ * The <code>ISalesOrderServiceRemote</code> bean exposes the business methods
+ * in the interface.
+ */
+public interface ISalesOrderServiceRemote {
+
+	// Sales Order
+
+	SalesOrder findSalesOrder(Long id)throws DoesNotExistException;
+	List<SalesOrder> findSalesOrders() throws DoesNotExistException;
+	void updateSalesOrder(SalesOrder salesOrder) throws BusinessException;
+	void addSalesOrder(SalesOrder salesOrder) throws BusinessException;
+	void addSalesOrderMaterial(Long id, SalesOrderMaterial salesordermaterial) throws BusinessException;
+	void logicalDeleteSalesOrder(SalesOrder salesOrder) throws BusinessException;
+	
+	// SalesOrderMaterial
+	
+	SalesOrderMaterial findSalesOrderMaterial(Long id)throws DoesNotExistException;
+	List<SalesOrderMaterial> findSalesOrderMaterialsBySalesOrderId(Long id) throws DoesNotExistException;
+	List<SalesOrderMaterial> findSalesOrderMaterialsBySalesOrder(SalesOrder salesOrder) throws DoesNotExistException;
+	
+	void updateSalesOrderMaterial(SalesOrderMaterial salesOrderMaterial) throws BusinessException;
+	void logicalDeleteSalesOrderMaterial(SalesOrderMaterial salesOrderMaterial) throws BusinessException;
+	
+	List<SalesOrder> findSalesOrdersBySearchFieldsRange(SalesOrderSearchFields lower, SalesOrderSearchFields upper,SearchOptions options) throws DoesNotExistException;
+	List<SalesOrder> findSalesOrdersBySearchFields(SalesOrderSearchFields searchFields,SearchOptions options) throws DoesNotExistException;
+	
+}
