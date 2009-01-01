cabb607a9c9766abfad8ddf2e02ebfa1d7bf95dd
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-01 10:41:19 +0000

    Initial commit of QERP_EJB files

---
+++ qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/production/iface/IProductionOrderServiceRemote.java
@@ -0,0 +1,65 @@
+package com.quesofttech.business.domain.production.iface;
+
+import java.util.List;
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.common.exception.DoesNotExistException;
+import com.quesofttech.business.domain.production.ProductionOrder;
+import com.quesofttech.business.domain.production.ProductionOrderOperation;import com.quesofttech.business.domain.production.ProductionOrderMaterial;
+
+/**
+ * The <code>IProductionOrderServiceRemote</code> bean exposes the business methods
+ * in the interface.
+ */
+public interface IProductionOrderServiceRemote {
+
+	// ProductionOrder
+
+	ProductionOrder findProductionOrder(Long id)throws DoesNotExistException;
+
+	List<ProductionOrder> findProductionOrders() throws DoesNotExistException;
+
+	void updateProductionOrder(ProductionOrder productionOrder) throws BusinessException;
+	
+	void addProductionOrder(ProductionOrder productionOrder) throws BusinessException;
+	
+	void logicalDeleteProductionOrder(ProductionOrder productionOrder) throws BusinessException;
+
+	
+	// ProductionOrderOperationOperation
+
+	ProductionOrderOperation findProductionOrderOperation(Long id)throws DoesNotExistException;
+
+	List<ProductionOrderOperation> findProductionOrderOperations() throws DoesNotExistException;
+
+	List<ProductionOrderOperation> findProductionOrderOperationsByProductionOrderId(Long ProductionOrderId) throws DoesNotExistException;
+	
+	List<ProductionOrderOperation> findProductionOrderOperationsByProductionOrder(ProductionOrder productionOrder) throws DoesNotExistException;
+	
+	void updateProductionOrderOperation(ProductionOrderOperation ProductionOrder) throws BusinessException;
+	
+	void addProductionOrderOperation(ProductionOrderOperation ProductionOrder) throws BusinessException;
+	
+	void logicalDeleteProductionOrderOperation(ProductionOrderOperation productionOrderOperation) throws BusinessException;
+
+	
+	
+
+	// ProductionOrderMaterial
+	
+	ProductionOrderMaterial findProductionOrderMaterial(Long id)throws DoesNotExistException;
+
+	List<ProductionOrderMaterial> findProductionOrderMaterials() throws DoesNotExistException;
+
+	void updateProductionOrderMaterial(ProductionOrderMaterial productionOrderMaterial) throws BusinessException;
+	
+	void addProductionOrderMaterial(long id,ProductionOrderMaterial productionOrderMaterial) throws BusinessException;
+	
+	void logicalDeleteProductionOrderMaterial(ProductionOrderMaterial productionOrderMaterial) throws BusinessException;
+	
+	List<ProductionOrderMaterial> findProductionOrderMaterialsByProductionOrderId(Long productionOrderId) throws DoesNotExistException;
+	
+	List<ProductionOrderMaterial> findProductionOrderMaterialsByProductionOrder(ProductionOrder productionOrder) throws DoesNotExistException;
+	
+	
+}
---
+++ qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/production/iface/IRoutingServiceLocal.java
@@ -0,0 +1,7 @@
+package com.quesofttech.business.domain.production.iface;
+
+/**
+ * The <code>IRoutingServiceLocal</code> bean exposes the business methods in the interface.
+ */
+public interface IRoutingServiceLocal extends IRoutingServiceRemote {
+}
