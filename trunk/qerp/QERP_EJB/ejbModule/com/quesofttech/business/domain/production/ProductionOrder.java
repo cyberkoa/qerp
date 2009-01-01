90fbd926b371aa9409c56663263ba21fc0c2e1c9
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-01 10:46:05 +0000

    Initial commit of QERP_EJB files

---
+++ trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/production/ProductionOrder.java
@@ -0,0 +1,529 @@
+package com.quesofttech.business.domain.production;
+
+import java.io.Serializable;
+//import java.sql.Date;
+//import java.sql.Time;
+import java.sql.Timestamp;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.PostLoad;
+import javax.persistence.PrePersist;
+import javax.persistence.PostPersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
+import javax.persistence.TableGenerator;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.Version;
+import javax.persistence.Embedded;
+//import javax.persistence.SequenceGenerator;
+
+import com.quesofttech.business.domain.inventory.Material;
+import com.quesofttech.business.domain.production.ProductionOrder;
+import com.quesofttech.business.domain.production.ProductionOrderMaterial;
+import com.quesofttech.business.domain.production.ProductionOrderOperation;
+
+import com.quesofttech.business.domain.sales.SalesOrder;
+
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.domain.base.BaseEntity;
+import com.quesofttech.business.domain.embeddable.RowInfo;
+import com.quesofttech.business.common.exception.ValueRequiredException;
+import com.quesofttech.util.StringUtil;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "ProductionOrder", uniqueConstraints = { @UniqueConstraint(columnNames = { "docNo" }) })
+@SuppressWarnings("serial")
+public class ProductionOrder extends BaseEntity {
+	
+
+	//For Postgresql : @SequenceGenerator(name = "ProductionOrder_sequence", sequenceName = "ProductionOrder_id_seq")
+	//Generic solution : (Use a table named primary_keys, with 2 fields , key &  value)
+	@TableGenerator(  name="ProductionOrder_id", table="PrimaryKeys", pkColumnName="tableName", pkColumnValue="ProductionOrder", valueColumnName="keyField")
+	@Id
+	//For Postgresql : @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ProductionOrder_sequence")
+	//For MSSQL      : @GeneratedValue(strategy = GenerationType.IDENTITY)
+	//Generic solution :
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "ProductionOrder_id")	
+	@Column(name = "id_ProductionOrder", nullable = false)
+	private Long id;
+	
+	@Version
+	@Column(nullable = false)
+	private Long version;
+
+	
+
+	@Column(name = "prodo_Number", length = 10, nullable = false)
+	private String docNo;
+	
+	
+	// Example of field	
+	@Column(name = "prodo_QuantityOrder", nullable = false)
+	private Double quantityOrder;
+		
+	@Column(name = "prodo_QuantityReported", nullable = false)
+	private Double quantityReported;
+	
+	
+	@ManyToOne
+    @JoinColumn(name="fk_Material")	
+	private Material material;	
+	
+	@ManyToOne
+    @JoinColumn(name="fk_SalesOrder")	
+	private SalesOrder salesOrder;	
+	
+	
+
+	
+	
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch = FetchType.EAGER, mappedBy="productionOrder", targetEntity=ProductionOrderMaterial.class)
+	private List<ProductionOrderMaterial> productionOrderMaterials;
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch = FetchType.EAGER, mappedBy="productionOrder", targetEntity=ProductionOrderOperation.class)
+	private List<ProductionOrderOperation> productionOrderOperations;	
+	
+	
+	@Embedded
+	RowInfo rowInfo;
+	
+	public ProductionOrder() {
+		super();
+		this.rowInfo = new RowInfo();
+		this.productionOrderMaterials = new ArrayList<ProductionOrderMaterial>();
+		this.productionOrderOperations = new ArrayList<ProductionOrderOperation>();
+	}	
+
+	
+	// Constructors without the common fields
+	//public ProductionOrder(/* all fields */) {
+	//	super();
+		/* Example of assignment
+		this.id = id;
+		this.type = type;
+		this.description = description;
+		*/
+	//}
+
+
+	// Constructors with all fields
+	public ProductionOrder(/* all fields*/
+			String recordStatus, String sessionId, String createLogin,
+			String createApp, Timestamp createTimestamp,
+			String modifyLogin, String modifyApp, Timestamp modifyTimestamp) {
+		this();
+
+		
+		this.id = id;
+		
+		/* Example of assignment
+		this.type = type;
+		this.description = description;
+		*/
+
+
+		this.rowInfo.setRecordStatus(recordStatus);
+		this.rowInfo.setSessionId(sessionId);
+		this.rowInfo.setCreateLogin(createLogin);
+		this.rowInfo.setCreateApp(createApp);
+		this.rowInfo.setCreateTimestamp(createTimestamp);
+		this.rowInfo.setModifyLogin(modifyLogin);
+		this.rowInfo.setModifyApp(modifyApp);
+		this.rowInfo.setModifyTimestamp(modifyTimestamp);
+	}
+
+
+	@Override
+	public String toString() {
+		
+		StringBuffer buf = new StringBuffer();
+		buf.append("ProductionOrder: [");
+		buf.append("id=" + id + ", ");
+
+		/* All fields
+		buf.append("type=" + type + ", ");
+		buf.append("description=" + description + ", ");
+		buf.append("isProduced=" + isProduced + ", ");
+		buf.append("isPurchased=" + isPurchased + ", ");
+		buf.append("isForSales=" + isForSale + ", ");
+		*/
+		// Common fields
+		buf.append("record status=" + rowInfo.getRecordStatus() + ", ");
+		buf.append("session id=" + rowInfo.getSessionId() + ", ");
+		buf.append("create login=" + rowInfo.getCreateLogin() + ", ");
+		buf.append("create app=" + rowInfo.getCreateApp() + ", ");
+		buf.append("create timestamp=" + rowInfo.getCreateTimestamp() + ", ");
+		buf.append("modify login=" + rowInfo.getModifyLogin() + ", ");
+		buf.append("modify app=" + rowInfo.getModifyApp() + ", ");
+		buf.append("modify time=" + rowInfo.getModifyTimestamp() + ", ");		
+		buf.append("version=" + version);
+		buf.append("]");
+		return buf.toString();		
+			
+	}
+
+	// The need for an equals() method is discussed at http://www.hibernate.org/109.html
+
+	@Override
+	public boolean equals(Object obj) {
+		return (obj == this) || (obj instanceof ProductionOrder) && getId() != null && ((ProductionOrder) obj).getId().equals(this.getId());
+	}
+
+	// The need for a hashCode() method is discussed at http://www.hibernate.org/109.html
+
+	@Override
+	public int hashCode() {
+		return getId() == null ? super.hashCode() : getId().hashCode();
+	}	
+	
+	@Override
+	public Serializable getIdForMessages() {
+		return getId();
+	}
+
+	
+	@PrePersist
+	void prePersist() throws BusinessException {
+		validate();
+		
+		rowInfo.setRecordStatus("A");
+		
+	    java.util.Date today = new java.util.Date();
+
+	    rowInfo.setModifyTimestamp(new java.sql.Timestamp(today.getTime()));
+	    rowInfo.setCreateTimestamp(rowInfo.getModifyTimestamp());			
+		System.out.println("[PrePersist] " + rowInfo.getCreateLogin());
+	}
+
+	@PostPersist
+	void postPersist() throws BusinessException {
+
+	}
+	
+	@PostLoad
+	void postLoad() {
+
+	}
+
+	@PreUpdate
+	void preUpdate() throws BusinessException {
+		if(rowInfo.getRecordStatus()!="D")
+		{
+			validate();
+		}
+			java.util.Date today = new java.util.Date();
+
+			rowInfo.setModifyTimestamp(new java.sql.Timestamp(today.getTime()));
+		
+		
+	}
+
+	@PreRemove
+	void preRemove() throws BusinessException {
+		// Check business rules here, eg.
+		// if (entity.getParts().size() > 0) {
+		// throw new CannotDeleteIsNotEmptyException(this, id, "Part");
+		// }
+
+		// There's no need to remove me from the parent(s) - that's the caller's
+		// responsibility (and for performance, it might not bother)
+	}
+
+	public void validate() throws BusinessException {
+
+		
+		// Validate syntax...
+
+		if (StringUtil.isEmpty(docNo)) {
+			//System.out.println("Yeah");
+			throw new ValueRequiredException(this, "ProductionOrder_DocNo");
+		}
+
+
+
+		/*
+		if (StringUtil.isEmpty(lastName)) {
+			throw new ValueRequiredException(this, "User_lastName");
+		}
+
+		// Validate semantics...
+
+		if (expiryDate != null && loginId.equals(ADMIN_LOGINID)) {
+			throw new GenericBusinessException("User_expirydate_not_permitted_for_user", new Object[] { ADMIN_LOGINID });
+		}
+
+		 */
+		//throw new ValueRequiredException(this, "Please remove this line");
+	}
+	
+	
+
+	// Common EJB field
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+
+
+
+	public String getRecordStatus() {
+		return rowInfo.getRecordStatus();
+	}
+
+
+
+	public void setRecordStatus(String recordStatus) {
+		this.rowInfo.setRecordStatus(recordStatus);
+	}
+
+
+
+	public String getSessionId() {
+		return rowInfo.getSessionId();
+	}
+
+
+	public void setSessionId(String sessionId) {
+		this.rowInfo.setSessionId(sessionId);
+	}
+
+
+
+	public String getCreateLogin() {
+		return rowInfo.getCreateLogin();
+	}
+
+
+
+	public void setCreateLogin(String createLogin) {
+		this.rowInfo.setCreateLogin(createLogin);
+	}
+
+
+
+	public String getCreateApp() {
+		return rowInfo.getCreateApp();
+	}
+
+
+
+	public void setCreateApp(String createApp) {
+		this.rowInfo.setCreateApp(createApp);
+	}
+
+
+
+	public java.sql.Timestamp getCreateTimestamp() {
+		return rowInfo.getCreateTimestamp();
+	}
+
+	public void setCreateTimestamp(java.sql.Timestamp createTimestamp) {
+		this.rowInfo.setCreateTimestamp(createTimestamp);
+	}
+
+
+	public String getModifyLogin() {
+		return rowInfo.getModifyLogin();
+	}
+
+	public void setModifyLogin(String modifyLogin) {
+		this.rowInfo.setModifyLogin(modifyLogin);
+	}
+
+
+
+	public String getModifyApp() {
+		return rowInfo.getModifyApp();
+	}
+
+
+
+	public void setModifyApp(String modifyApp) {
+		this.rowInfo.setModifyApp(modifyApp);
+	}
+
+
+	public java.sql.Timestamp getModifyTimestamp() {
+		return rowInfo.getModifyTimestamp();
+	}
+
+
+
+	public void setModifyTimestamp(java.sql.Timestamp modifyTimestamp) {
+		this.rowInfo.setModifyTimestamp(modifyTimestamp);
+	}
+
+
+
+
+	/**
+	 * @return the quantityOrder
+	 */
+	public Double getQuantityOrder() {
+		return quantityOrder;
+	}
+
+
+	/**
+	 * @param quantityOrder the quantityOrder to set
+	 */
+	public void setQuantityOrder(Double quantityOrder) {
+		this.quantityOrder = quantityOrder;
+	}
+
+
+	/**
+	 * @return the quantityReported
+	 */
+	public Double getQuantityReported() {
+		return quantityReported;
+	}
+
+
+	/**
+	 * @param quantityReported the quantityReported to set
+	 */
+	public void setQuantityReported(Double quantityReported) {
+		this.quantityReported = quantityReported;
+	}
+
+
+	/**
+	 * @return the material
+	 */
+	public Material getMaterial() {
+		return material;
+	}
+
+
+	/**
+	 * @param material the material to set
+	 */
+	public void setMaterial(Material material) {
+		this.material = material;
+	}
+
+
+	/**
+	 * @return the productionOrderMaterials
+	 */
+	public List<ProductionOrderMaterial> getProductionOrderMaterials() {
+		return productionOrderMaterials;
+	}
+
+
+	/**
+	 * @param productionOrderMaterials the productionOrderMaterials to set
+	 */
+	public void setProductionOrderMaterials(List<ProductionOrderMaterial> productionOrderMaterials) {
+		this.productionOrderMaterials = productionOrderMaterials;
+	}
+
+
+	/**
+	 * @return the docNo
+	 */
+	public String getDocNo() {
+		return docNo;
+	}
+
+
+	/**
+	 * @param docNo the docNo to set
+	 */
+	public void setDocNo(String docNo) {
+		this.docNo = docNo;
+	}
+
+
+	/**
+	 * @return the salesOrder
+	 */
+	public SalesOrder getSalesOrder() {
+		return salesOrder;
+	}
+
+
+	/**
+	 * @param salesOrder the salesOrder to set
+	 */
+	public void setSalesOrder(SalesOrder salesOrder) {
+		this.salesOrder = salesOrder;
+	}
+
+
+	/**
+	 * @return the productionOrderOperations
+	 */
+	public List<ProductionOrderOperation> getProductionOrderOperations() {
+		return productionOrderOperations;
+	}
+
+
+	/**
+	 * @param productionOrderOperations the productionOrderOperations to set
+	 */
+	public void setProductionOrderOperations(List<ProductionOrderOperation> productionOrderOperations) {
+		this.productionOrderOperations = productionOrderOperations;
+	}
+
+	
+	public void addProductionOrderMaterial(ProductionOrderMaterial productionOrderMaterial) {
+		if (!productionOrderMaterials.contains(productionOrderMaterial)) {
+			productionOrderMaterials.add(productionOrderMaterial);
+			/* Maintain the bidirectional relationship . */
+			productionOrderMaterial.setProductionOrder(this);
+		}
+	}
+
+	public void addProductionOrderOperation(ProductionOrderOperation productionOrderOperation) {
+		if (!productionOrderOperations.contains(productionOrderOperation)) {
+			productionOrderOperations.add(productionOrderOperation);
+			/* Maintain the bidirectional relationship . */
+			productionOrderOperation.setProductionOrder(this);
+		}
+	}
+
+	public String getMaterialCode()
+	{
+		if (material!=null) return material.getCode();
+		else return "";
+	}
+	public String getSalesOrderDoc()
+	{
+		if(salesOrder!=null) return salesOrder.getDocNo();
+		else return "";
+	}
+	
+}
---
+++ trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/production/RoutingService.java
@@ -0,0 +1,77 @@
+package com.quesofttech.business.domain.production;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.common.exception.DoesNotExistException;
+import com.quesofttech.business.domain.base.BaseService;
+import com.quesofttech.business.domain.production.Routing;
+import com.quesofttech.business.domain.production.iface.IRoutingServiceLocal;
+import com.quesofttech.business.domain.production.iface.IRoutingServiceRemote;
+
+@Stateless
+@Local(IRoutingServiceLocal.class)
+@Remote(IRoutingServiceRemote.class)
+public class RoutingService extends BaseService implements IRoutingServiceLocal, IRoutingServiceRemote {
+
+//	@PersistenceContext(unitName = "QERP_EJB")
+//	protected EntityManager _em;
+
+	// Routing
+
+	public Routing findRouting(Long id) throws DoesNotExistException {
+		Routing routing = (Routing) find(Routing.class, id);
+		return routing;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Routing> findRoutings() throws DoesNotExistException {
+		Query q = _em.createQuery("select routing from Routing routing order by routing.id");
+		List l = q.getResultList();
+		return l;
+	}
+/*
+	public Routing findRoutingByType(String type) {
+		Routing routing = _em.find(Routing.class, id);
+		return routing;
+	}
+*/
+	
+	
+	public void updateRouting(Routing routing) throws BusinessException {		
+		routing = (Routing) merge(routing);
+	}
+
+	public void logicalDeleteRouting(Routing routing) throws BusinessException {
+		routing.rowInfo.setRecordStatus("D");
+		updateRouting(routing);
+	}
+	
+	public void addRouting(Routing routing) throws BusinessException {
+		
+		//try{
+		System.out.println("just before persist in RoutingService");
+		persist(routing);
+		
+		System.out.println("just after persist in RoutingService");
+		//}
+	/*	catch (java.lang.RuntimeException e)
+		{
+			System.out.println("[RoutingService] RuntimeException caught in persist");
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println("[addRouting] : exception caught during persist" + e.getMessage());
+			throw (BusinessException) e;
+		}
+		*/
+	}
+}
