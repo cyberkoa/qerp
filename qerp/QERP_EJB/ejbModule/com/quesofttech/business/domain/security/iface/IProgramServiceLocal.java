39ba9c83a61bafc5cd5431d4795d09aadc569750
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-01 10:57:15 +0000

    Initial commit of QERP_EJB files

---
+++ qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/security/iface/IProgramServiceLocal.java
@@ -0,0 +1,7 @@
+package com.quesofttech.business.domain.security.iface;
+
+/**
+ * The <code>IProgramServiceLocal</code> bean exposes the business methods in the interface.
+ */
+public interface IProgramServiceLocal extends IProgramServiceRemote {
+}
