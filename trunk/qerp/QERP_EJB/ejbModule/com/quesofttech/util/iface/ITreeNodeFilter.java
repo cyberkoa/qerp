cc510735c937498aaeb73a5d46de22bdcfd6ff08
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-01-18 04:36:16 +0000

    Add in delegate function to filter node in bom explosion

---
+++ trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/util/iface/ITreeNodeFilter.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.quesofttech.util.iface;
+
+import com.quesofttech.util.TreeNode;
+
+/**
+ * @author super
+ *
+ */
+public interface ITreeNodeFilter {
+	boolean filter(Object obj);
+}
