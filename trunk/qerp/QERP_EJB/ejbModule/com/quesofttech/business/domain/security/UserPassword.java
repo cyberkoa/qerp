38946c7065bfb8cd5edb1f6146ad0150694d9435
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-03-02 10:29:28 +0000

    Fix bug in the User,UserPassword, that @TableGenerator and @GeneratedValue is not sync , in the previous commit

--- trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/security/UserPassword.java
+++ trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/security/UserPassword.java
@@ -39,7 +39,7 @@ public class UserPassword extends BaseEntity {
 	}	
 	//For Postgresql : @SequenceGenerator(name = "material_sequence", sequenceName = "material_id_seq")
 	//Generic solution : (Use a table named PrimaryKeys, with 2 fields , tableName &  keyField)
-	@TableGenerator(  name="userpassword_id", table="PrimaryKeys", pkColumnName="tableName", pkColumnValue="UserPassword", valueColumnName="keyField")	
+	@TableGenerator(  name="UserPassword_id", table="PrimaryKeys", pkColumnName="tableName", pkColumnValue="UserPassword", valueColumnName="keyField")	
 	@Id
 	//For Postgresql : @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userpassword_sequence")
 	//For MSSQL      : @GeneratedValue(strategy = GenerationType.IDENTITY)
