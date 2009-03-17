9ca1395061c3b1ca525f699b76a8cbb97219e339
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-03-17 15:03:44 +0000

    Add UserRoleService and related interface class

---
+++ trunk/qerp/QERP_EJB/ejbModule/com/quesofttech/business/domain/security/iface/IUserRoleServiceRemote.java
@@ -0,0 +1,27 @@
+package com.quesofttech.business.domain.security.iface;
+
+import java.util.List;
+
+import com.quesofttech.business.common.exception.BusinessException;
+import com.quesofttech.business.common.exception.DoesNotExistException;
+import com.quesofttech.business.domain.security.UserRole;
+
+/**
+ * The <code>IUserRoleServiceRemote</code> bean exposes the business methods
+ * in the interface.
+ */
+public interface IUserRoleServiceRemote {
+
+	// UserRole
+
+	UserRole findUserRole(Long id)throws DoesNotExistException;
+
+	List<UserRole> findUserRoles() throws DoesNotExistException;
+
+	void updateUserRole(UserRole userRole) throws BusinessException;
+	
+	void addUserRole(UserRole userRole) throws BusinessException;
+	
+	void logicalDeleteUserRole(UserRole userRole) throws BusinessException;
+
+}
