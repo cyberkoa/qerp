7aa6f2ff7671afc18a194a126676b516d3f1cd8a
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-04-08 12:15:36 +0000

    Add in DataPage and MaintenancePage

--- branches/v030/qerp/QERP_Web/src/com/quesofttech/web/base/DataPage.java
+++ branches/v030/qerp/QERP_Web/src/com/quesofttech/web/base/DataPage.java
@@ -2,13 +2,59 @@
 package com.quesofttech.web.base;
 
 import com.quesofttech.web.base.SecureBasePage;
+import com.quesofttech.web.components.QERPWindow;
 
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.Retain;
+import org.apache.tapestry5.beaneditor.BeanModel;
+import org.apache.tapestry5.internal.grid.CollectionGridDataSource;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.BeanModelSource;
+import org.slf4j.*;
 
 public class DataPage extends SecureBasePage 
 {
 	
+	protected Logger logger; 
+	
+	protected CollectionGridDataSource collectionGridDataSource;
+	
+	@Inject
+    protected BeanModelSource beanModelSource;
+	
+	@SuppressWarnings("unchecked")
+	@Property
+	@Retain
+	protected BeanModel beanModel;
+	
+	@Inject
+    protected ComponentResources resources;
 	
 	
+	// This may replace the filter
+	@SuppressWarnings("unchecked")
+	@Property
+	@Retain	
+	protected BeanModel filterModel;
+	
+	/* cyberkoa : make a dynamic filterWindow, with dynamic size 
+	@Component(parameters = {"width=300", "height=300", 
+			                 "style=bluelighting", "show=false",
+			                 "modal=true", "title=literal:Filter Record"})
+	@Property
+	private QERPWindow _filterWindow;
+	*/
+	
+	//protected Viewer viewer;
+	
+	// protected Grid grid;
+	
+	// protected Toolbar toolbar;
+	
+	// Methods
+	 
 	
 	
 }
---
+++ branches/v030/qerp/QERP_Web/src/com/quesofttech/web/base/MaintenancePage.java
@@ -0,0 +1,14 @@
+
+package com.quesofttech.web.base;
+
+import com.quesofttech.web.base.SecureBasePage;
+
+
+public class MaintenancePage extends DataPage 
+{
+	
+	
+	
+	
+	
+}
