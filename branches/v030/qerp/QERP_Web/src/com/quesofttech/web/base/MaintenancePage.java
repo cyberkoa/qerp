fc1c509a86965a48f34bc0dfa5cda2af1deb4871
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-04-26 16:40:00 +0000

    Add in method prototypes for DataPage and Maintenance page

--- branches/v030/qerp/QERP_Web/src/com/quesofttech/web/base/MaintenancePage.java
+++ branches/v030/qerp/QERP_Web/src/com/quesofttech/web/base/MaintenancePage.java
@@ -7,8 +7,31 @@ import com.quesofttech.web.base.SecureBasePage;
 public class MaintenancePage extends DataPage 
 {
 	
+	protected Boolean addRecord()
+	{	
+		return true;
+	}
 	
+	protected Boolean editRecord()
+	{
 	
+		return true;
+	}
+	
+	protected Boolean saveRecord()
+	{
+		return true;
+	}
+	
+	protected Boolean deleteRecord()
+	{
+		return true;
+	}
+	
+	protected Boolean updateRecord()
+	{
+		return true;
+	}
 	
 	
 }
