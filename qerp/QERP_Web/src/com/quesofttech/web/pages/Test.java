fcddab7c22be792cf8d85b7f34ef3cdf5e2f0ffc
cyberkoa <cyberkoa@39e80d1e-d7e2-11dd-90d0-c9c6575c7768>
2009-08-10 18:29:06 +0000

    Update the latest Test app

--- qerp/QERP_Web/src/com/quesofttech/web/pages/Test.java
+++ qerp/QERP_Web/src/com/quesofttech/web/pages/Test.java
@@ -1,5 +1,6 @@
 package com.quesofttech.web.pages;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Date;
 import java.sql.Timestamp;
 import java.util.List;
@@ -32,7 +33,7 @@ import org.apache.tapestry5.annotations.ApplicationState;
 import com.quesofttech.web.components.*;
 
 
-public class Test {
+public class Test extends SimpleBasePage {
 	//@Parameter(defaultPrefix="literal", required=true)
     //private String _pages;
 
@@ -43,23 +44,22 @@ public class Test {
 
     private StandardToolbar toolbar;
 
+    @Inject
     private Block blockFormView;
+
+    @Inject
+    private Block blockGridView;
     
-    private int testvalue;
+    private Zone zoneFormView;
+
+    private Zone zoneGridView;
+    
+    private String testvalue;
+
+    private Operation _operation;
+    private Collection<?> _operations;
 
 
-
-	public int getTestvalue()
-	{
-		return testvalue;
-	}
-
-
-
-	public void setTestvalue(int testvalue)
-	{
-		this.testvalue = testvalue;
-	}
     
 	@OnEvent(value = "addRecord", component = "toolbar")
     Object onAddRecord()
@@ -67,6 +67,112 @@ public class Test {
 		System.out.println("triggered addRecord");
     	return blockFormView;
     }
+
+
+
+	public String getTestvalue()
+	{
+		return testvalue;
+	}
+
+
+
+	public void setTestvalue(String testvalue)
+	{
+		this.testvalue = testvalue;
+	}
+
+
+
+	public Block getBlockFormView()
+	{
+		return blockFormView;
+	}
+
+
+
+	public void setBlockFormView(Block blockFormView)
+	{
+		this.blockFormView = blockFormView;
+	}
+
+
+
+	public Block getBlockGridView()
+	{
+		return blockGridView;
+	}
+
+
+
+	public void setBlockGridView(Block blockGridView)
+	{
+		this.blockGridView = blockGridView;
+	}
+    	
+	
+    public Zone getZoneFormView()
+	{
+		return zoneFormView;
+	}
+
+
+
+	public void setZoneFormView(Zone zoneFormView)
+	{
+		this.zoneFormView = zoneFormView;
+	}
+
+
+
+	private IOperationServiceRemote getOperationService() {
+        return getBusinessServicesLocator().getOperationServiceRemote();
+     }
+
+
+     public Collection<?> getOperations() {
+        return _operations;
+     }
+
+
+
+	public Operation getOperation()
+	{
+		return _operation;
+	}
+
+
+
+	public void setOperation(Operation operation)
+	{
+		this._operation = operation;
+	}
+     
+     
+	
+    void setupRender() {
+        loadRecords();
+     }
+	
+	
+    void loadRecords()
+    {
+       try
+       {
+           _operations = getOperationService().findOperations();
+       }
+       catch(BusinessException be)
+       {
+
+       }
+       if(_operations!=null && !_operations.isEmpty())
+       {
+    	   Operation[] a = new Operation[_operations.size()];
+    	   this._operation = _operations.toArray(a)[0];
+    	   this.setTestvalue(_operation.getCode());
+       }
+    }
     
+
     
 }
